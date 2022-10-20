56a6dbfeef94af30276aa493d69a360fb33bd9c6
Guillaume Leroi <guillaume@leroi.re>
2022-10-21 00:30:45 +0200

    move to lib.rs

--- src/vm/src/lib.rs
+++ src/vm/src/lib.rs
@@ -1,8 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use syscalls::{Sysno, syscall, Errno};
+use std::ffi::CString;
+
+pub fn mount(source: &CString, target: &CString, fstype: &CString) -> Result<usize, Errno> {
+    unsafe {
+        syscall!(Sysno::mount, source.as_ptr(), target.as_ptr(), fstype.as_ptr(), 0, std::ptr::null() as *const i8)
     }
 }
--- src/vm/src/main.rs
+++ src/vm/src/main.rs
@@ -1,12 +1,7 @@
-use syscalls::{Sysno, syscall, Errno};
 use std::ffi::CString;
 use std::error::Error;
+use vm::mount;
 
-fn mount(source: &CString, target: &CString, fstype: &CString) -> Result<usize, Errno> {
-    unsafe {
-        syscall!(Sysno::mount, source.as_ptr(), target.as_ptr(), fstype.as_ptr(), 0, std::ptr::null() as *const i8)
-    }
-}
 
 fn main() -> Result<(), Box<dyn Error>>{
     let file = CString::new("/tmp/img.raw")?;
